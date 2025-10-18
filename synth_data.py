15e4b9925c523cfcf468f3728ae77417ff02dab3
Trevor Nei <t@trevornei.com>
2025-10-18 20:25:16 +0000

    Created a fn that uses a for loop to create synthetic data for users table each iteration

--- synth_data.py
+++ synth_data.py
@@ -2,5 +2,29 @@ from faker import Faker
 
 faker = Faker()
 
-gen_name = faker.name()
-print(f"Testing out creating a fake name: {gen_name}")
+# gen_name = faker.name()
+# print(f"Testing out creating a fake name: {gen_name}")
+
+"""
+For Users & Feeds
+1 Create for loop that iterates to 10
+2 for each iterate call faker.methods() to generate synthetic data.
+3 sequentially append the data so that it conforms to the data model. 
+"""
+
+# Create a list of dictionaries for user and key value pairs that map to the data model. 
+
+users = []
+
+def generate_users():
+  for i in range(10):
+      first_name = faker.first_name()
+      last_name = faker.last_name()
+      username = faker.user_name()
+      print(f"""
+          First_Name = {first_name}
+          Last_Name = {last_name}
+          username = {username}
+      """)
+
+generate_users()
