1cdd353dd6bb0f9f962cd2c621ebc778bdb9f4b5
trevornei <117675657+trevornei@users.noreply.github.com>
2025-11-09 09:06:56 -0700

    Added .env to .gitignore and cleaned up project structure.

--- py_projects/orm_cli.py
+++ py_projects/orm_cli.py
@@ -2,7 +2,7 @@ import mysql.connector
 from dotenv import dotenv_values
 from mysql.connector import (connection)
 
-creds = dotenv_values("../.env")
+creds = dotenv_values("./.env")
 print(creds)
 
 # Create vars: host, user, password, database
@@ -11,17 +11,11 @@ host = creds["host"]
 user = creds["user"]
 password = creds["password"]
 database = creds["database"]
-unix_socket = creds["unix_socket"]
 
-# print(f"The value of Host: {host}\n The value of User: {user}\n The value of PW: {password}\n The value DB: {database}")
-
-# Connects to the database with IPC path(Unix Interprocess communication)
-# I found out that I have to have a variable for the unix_socket if I'm using loopback local for the host address. 
-#   ---> this is because mysqlconnect library defaults to using tcp instead of ICP.
-cnx = connection.MySQLConnection(host=host, user=user, password=password, database=database, unix_socket=unix_socket)
+cnx = connection.MySQLConnection(host=host, user=user, password=password, database=database)
 curse = cnx.cursor()
 
-print("Welcome and wake up Neo.")
+print("Assignment: ORM CLI to the Sakila Database.")
 print(f"You know have access to the {database}.\n You know who the birds work for.")
 
 name = input("What is your name Neo?")
